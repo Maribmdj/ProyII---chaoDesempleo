a0f8cd339b6d58a0e679a889adf4fb9debc0a055
Maribmdj <maribmdj@gmail.com>
2018-11-03 16:30:02 -0600

    Cambios
    Mariela

--- SCRIPTS/PACKAGES/INSERTS/insert_user.sql
+++ SCRIPTS/PACKAGES/INSERTS/insert_user.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE PROCEDURE insert_user(IN puser INT, IN ppassword VARCHAR(100), IN pid_person INT)
+CREATE PROCEDURE insert_user(IN puser VARCHAR(80), IN ppassword VARCHAR(100), IN pid_person INT)
 	BEGIN
 		INSERT INTO user(user, password, id_person) VALUES (puser, ppassword, pid_person);
     END$$
