75b037fb28645e379260c362aff6cfc93d1fafb3
Bubby <oldowl12@gmail.com>
2018-11-04 00:40:22 -0600

    LOGIN FUNCIONA, CLASE DE MANEJO DE DB AGREGADA
    Se agregó una funcion de validación de usuario.
    Se cambió la estructura del proyecto en java.
    Se arreglaron algunos inserts.

---
+++ SCRIPTS/CONSULTAS/validateUser.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS validateUser$$
+
+create function validateUser(username varchar(70), ppassword varchar(100))
+RETURNS INT
+READS SQL DATA
+DETERMINISTIC
+BEGIN
+	DECLARE i INT;
+	select id_user from user where user = username and password = ppassword INTO i;
+	if ((select i)!= 0)
+	then
+		return (select i);
+	else
+		return -1; 
+	end if;
+end $$
+DELIMITER ;
