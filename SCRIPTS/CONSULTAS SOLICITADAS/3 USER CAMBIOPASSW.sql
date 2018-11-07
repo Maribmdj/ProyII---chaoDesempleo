05948492f60f605fbbaead2aa1ad298de150f780
Maribmdj <maribmdj@gmail.com>
2018-11-06 20:41:57 -0600

    Consultas
    Mariela

--- SCRIPTS/CONSULTAS SOLICITADAS/3 USER CAMBIOPASSW.sql
+++ SCRIPTS/CONSULTAS SOLICITADAS/3 USER CAMBIOPASSW.sql
@@ -2,7 +2,8 @@ SELECT U.USER AS USUARIO, P.name AS NOMBRE, P.lastname1 AS APELLIDO, P.lastname2
 FROM
     USER U INNER JOIN PERSON P
     ON U.ID_PERSON = P.ID_PERSON 
-		AND (P.NAME LIKE CONCAT('%','','%') or P.LASTNAME1 LIKE CONCAT('%','','%') or P.LASTNAME2 LIKE CONCAT('%','','%')
-        #AND P.NUMBER_IDENTF= LIKE CONCAT('%','','%')
-        or U.USER LIKE CONCAT('%','','%'))
+		AND (P.NAME LIKE CONCAT('%','','%') or P.LASTNAME1 LIKE CONCAT('%','','%') or P.LASTNAME2 LIKE CONCAT('%','','%') or U.USER LIKE CONCAT('%','','%'))
+        #AND P.NUMBER_IDENTF= LIKE CAST(CONCAT('%','1529','%') as INT)
+        
+        
     #INNER JOIN BUSER BU ON BU.DATE_ BETWEEN pEDAD1 AND pEDAD2)
